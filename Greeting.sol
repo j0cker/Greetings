8abf78ab18a2f9d3a745bf2ae3eefeb727ca7109
j0cker <manlioelnum1@hotmail.com>
2018-01-14 10:16:16 -0600

    initial version

---
+++ Greeting.sol
@@ -0,0 +1,19 @@
+pragma solidity ∧0.4.11;
+
+contract Greetings {
+
+  string message;
+
+  function Greetings(){
+      messsage = "I am ready!";
+  }
+
+  function  setGreetings(string _message) public {
+      message = _message;
+  }
+
+  function getGreetings() constant returns (string){
+      return message;
+  }
+
+}
